1725c5971ebced772229ba95e4bef22bd05d129d
Hayeon LEE <fhdodl@naver.com>
2024-01-29 00:44:43 +0900

    Time: 63 ms (54.54%), Space: 53 MB (5.76%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -10,15 +10,15 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
-    if (head === null || head.next === null) {
-        return head;
+    var current = head;
+    
+    while(current) {
+        if(current.next !== null && current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
     }
-    if (head.val === head.next.val) {
-        head.next = head.next.next;
-        return deleteDuplicates(head);
-    } else {
-        head.next = deleteDuplicates(head.next);
-        return head;
-    }
+    
+    return head;
 };
