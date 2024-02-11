a16f17648756c47fc8c0d31016163f28603c8051
Hayeon LEE <fhdodl@naver.com>
2024-02-11 16:43:23 +0900

    Time: 55 ms (66.03%), Space: 49.1 MB (5.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,10 +2,18 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if(n<=1){
-        return n;
-    }else{
-        return fib(n-1) + fib( n-2);
-    }
+var fib = function (n) {
+  if (n == 0) return 0;
+
+  let prev = 0,
+    cur = 1,
+    temp;
+
+  for (let i = 1; i < n; i++) {
+    temp = cur;
+    cur = cur + prev;
+    prev = temp;
+  }
+
+  return cur;
 };
