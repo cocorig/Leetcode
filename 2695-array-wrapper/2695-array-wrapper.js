f3a9849235d8673ef9c4f5719294c491f96ec00d
Hayeon LEE <fhdodl@naver.com>
2024-05-06 22:57:38 +0900

    Time: 69 ms (6.16%), Space: 50.2 MB (64.76%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.arr = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    let Sum = 0;
+    let arr1 = this.arr;
+    for (v of arr1) {
+        Sum += v;
+    }
+    return Sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.arr);
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
